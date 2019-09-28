850abfbc78df8c7e3940c8565390103ef6979c9b
Nadzmi Idzham <nadzmiidzham@gmail.com>
2019-09-28 20:50:59 +0800

    finished

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,37 @@
 |
 */
 
+use App\User;
+use App\Address;
+
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/insert', function() {
+    $user = User::findOrFail(1);
+    $address = new Address(['name' => '1234 Houston Avenue']);
+
+    $user->address()->save($address);
+});
+
+Route::get('/update', function() {
+    $address = Address::where('user_id', '=', 1)->first();
+    // $address = Address::where('user_id', 1)->first();
+    // $address = Address::whereUserId(1)->first();
+
+    $address->name = 'Updated address';
+    $address->save();
+});
+
+Route::get('/read', function() {
+    $user = User::findOrFail(1);
+
+    echo $user->address->name;
+});
+
+Route::get('/delete', function() {
+    $user = User::findOrFail(1);
+
+    $user->address()->delete();
 });
